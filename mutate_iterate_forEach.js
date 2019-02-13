9401cc71c2f4055fe3faa8eb4eba30866aefdf08
Leigh Mathieson <leigh.mathieson@gmail.com>
2019-02-13 17:03:24 +0000

    minor diffs

--- mutate_iterate_forEach.js
+++ mutate_iterate_forEach.js
@@ -1,9 +1,9 @@
-let values = ["A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8"];
+const values = ["A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8"];
 let count = 0;
 values.slice().forEach((value, index) => {
   if (value === "A2" || value === "A5") {
     values.splice(index - count++, 1);
-  };
+  }
   // replaces value
   if (value === "A3" || value === "A4" || value === "A7") {
     values.splice(index, 1, 'newVal');
@@ -11,11 +11,11 @@ values.slice().forEach((value, index) => {
   // insert value before
   if (value === "A0" || value === "A6" || value === "A8") {
     values.splice(index - count--, 0, 'newVal');
-  };
+  }
   // insert value after
   if (value === "A0" || value === "A6" || value === "A8") {
     values.splice(index - --count, 0, 'newVal');
-  };
+  }
 });
 console.log(values);
 
